26f8f8d835128de658676eb00933d883c33827ee
JLee <jamieleevianzon@gmail.com>
2025-07-14 00:59:26 +0800

    Refactored Models

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ EFFICIENTNETB0_CONVOUT_SIZE = 320 * 4
 
 class CustomCNN(torch.nn.Module):
     def __init__(self, in_features=256, out_features=1):
-        super(CustomCNN,self).__init__()
+        super().__init__()
         self.model = torch.nn.Linear(in_features=in_features,out_features=out_features)
 
     def forward(self,x):
@@ -30,27 +30,28 @@ class CustomCNN(torch.nn.Module):
 
 class VGG19(torch.nn.Module):
     def __init__(self,
-                 classifier : Optional[torch.nn.Sequential] = None,
+                 classifier : Optional[torch.nn.Linear] = None,
                  dropout : float = .5,
                  freeze=True ,
                  pretrained=False
         ):
-        super(VGG19,self).__init__()
+        super().__init__()
+
+        # Change the last layer instead to preserve the other parameters
+        # if classifier == None:
+        #     classifier = torch.nn.Sequential(
+        #             torch.nn.Linear(VGG19_FEATURE_SIZE, 4096),
+        #             torch.nn.ReLU(True),
+        #             torch.nn.Dropout(p=dropout),
+        #             torch.nn.Linear(4096, 4096),
+        #             torch.nn.ReLU(True),
+        #             torch.nn.Dropout(p=dropout),
+        #             torch.nn.Linear(4096, BINARY_CLASSIFICATION),
+        #     )
 
         if classifier == None:
-            classifier = torch.nn.Sequential(
-                    torch.nn.Linear(VGG19_FEATURE_SIZE, 4096),
-                    torch.nn.ReLU(True),
-                    torch.nn.Dropout(p=dropout),
-                    torch.nn.Linear(4096, 4096),
-                    torch.nn.ReLU(True),
-                    torch.nn.Dropout(p=dropout),
-                    torch.nn.Linear(4096, BINARY_CLASSIFICATION),
-            )
+            classifier = torch.nn.Linear(4096, BINARY_CLASSIFICATION)
 
-        weights = None
-
-        
         weights = models.VGG19_Weights.DEFAULT if pretrained else None
 
         self.model = models.vgg19(weights=weights)
@@ -60,7 +61,7 @@ class VGG19(torch.nn.Module):
             for param in self.model.parameters():
                 param.requires_grad = False
 
-        self.model.classifier = classifier
+        self.model.classifier[-1] = classifier
 
     def forward(self, x):
         x = self.model(x)
@@ -75,14 +76,11 @@ class DenseNet121(torch.nn.Module):
                  pretrained = False
         ):
 
-        super(DenseNet121,self).__init__()
+        super().__init__()
 
         if classifier == None:
             classifier = torch.nn.Linear(DENSENET121_FEATURE_SIZE, BINARY_CLASSIFICATION)
 
-        weights = None
-
-        
         weights = models.VGG16_Weights.DEFAULT if pretrained else None
 
         self.model = models.densenet121(weights=weights)
@@ -105,14 +103,11 @@ class ResNet50(torch.nn.Module):
                  pretrained = False
         ):
         
-        super(ResNet50,self).__init__()
+        super().__init__()
 
         if classifier == None:
             classifier = torch.nn.Linear(RESNET50_FEATURE_SIZE, BINARY_CLASSIFICATION)
 
-        weights = None
-
-        
         weights = models.ResNet50_Weights.DEFAULT if pretrained else None
         
         self.model = models.resnet50(weights=weights)
@@ -137,14 +132,11 @@ class GoogLeNet(torch.nn.Module):
                  freeze = True,
                  pretrained = False
         ):
-        super(GoogLeNet,self).__init__()
+        super().__init__()
 
         if classifier == None:
             classifier = torch.nn.Linear(GOOGLENET_FEATURE_SIZE, BINARY_CLASSIFICATION)
 
-        weights = None
-
-        
         weights = models.GoogLeNet_Weights.DEFAULT if pretrained else None
         
         self.model = models.googlenet(weights=weights)
@@ -162,22 +154,23 @@ class GoogLeNet(torch.nn.Module):
 
 class EfficientNetB0(torch.nn.Module):
     def __init__(self, 
-                 classifier: Optional[torch.nn.Sequential] = None,
+                 classifier: Optional[torch.nn.Linear] = None,
                  dropout = 0.5,
                  freeze = True, 
                  pretrained = False
         ):
-        super(EfficientNetB0,self).__init__()
+        super().__init__()
+
+        # Change the last layer instead to preserve the other parameters
+        # if classifier == None:
+        #     classifier = torch.nn.Sequential(
+        #                  torch.nn.Dropout(p=dropout, inplace=True),
+        #                  torch.nn.Linear(EFFICIENTNETB0_CONVOUT_SIZE, BINARY_CLASSIFICATION),
+        #     )
 
         if classifier == None:
-            classifier = torch.nn.Sequential(
-                         torch.nn.Dropout(p=dropout, inplace=True),
-                         torch.nn.Linear(EFFICIENTNETB0_CONVOUT_SIZE, BINARY_CLASSIFICATION),
-            )
+                classifier = torch.nn.Linear(EFFICIENTNETB0_CONVOUT_SIZE, BINARY_CLASSIFICATION)
 
-        weights = None
-
-        
         weights = models.EfficientNet_B0_Weights.DEFAULT if pretrained else None
         
         self.model = models.efficientnet_b0(weights=weights)
@@ -187,7 +180,7 @@ class EfficientNetB0(torch.nn.Module):
             for param in self.model.parameters():
                 param.requires_grad = False
         
-        self.model.classifier = classifier
+        self.model.classifier[-1] = classifier
 
     def forward(self, x):
         x = self.model(x)
