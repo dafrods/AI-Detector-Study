e2c5580db424ee6773cd6ae2217c66cb912b8c2a
JLee <jamieleevianzon@gmail.com>
2025-07-14 13:54:01 +0800

    updated sources

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import torch
+import kagglehub
+
+path = kagglehub.dataset_download("manjilkarki/deepfake-and-real-images")
+
+print("Path to dataset files:", path)
 
 def get_device():
     if torch.cuda.is_available():
