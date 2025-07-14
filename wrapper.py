eba6f2e8c9b65914676613724b4f0d024a3fa671
JLee <jamieleevianzon@gmail.com>
2025-07-14 13:15:38 +0800

    Dataset Driver Complete

--- wrapper.py
+++ wrapper.py
@@ -14,7 +14,15 @@ from torch.optim import Optimizer
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
-from typing import Optional
+from PIL import Image
+
+import optuna
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+
+from typing import Optional, Literal
 
 class ClassificationModelTrainer:
 
@@ -92,28 +100,79 @@ class ImageDataset(Dataset):
 
     KINDS = ['Validation', 'Train', 'Test']
 
-    def __init__(self, path:Optional[str] = None, limit:Optional[int] = None, kind = ""):
+    def __init__(self, 
+                 path:Optional[str] = None, 
+                 limit:Optional[int] = None, 
+                 kind : Literal['Validation', 'Train', 'Test'] = "Train", 
+                 transform=None
+        ):
         
         # Start Parameter Processing
         if path == None:
-            dataset_root = r'./Dataset'
-        
-        if kind == None:
-            kind = "Training"
+            self.dataset_root = 'Dataset'
         
         if kind in ImageDataset.KINDS:
             self.kind = kind
         else:
-            raise ValueError("Kind must be one of the following: 'Validation', 'Train' (DEFAULT), 'Test' ")
-        # End Parameter Processing
-
-        self.len = len(os.listdir('Fake')) + len(os.listdir('Real'))
+            raise ValueError(f"Value of Kind {kind} Kind must be one of the following: 'Validation', 'Train' (DEFAULT), 'Test' ")
         
+        self.transform = transform
+        # End Parameter Processing
+        # print(os.getcwd())
+        # print(Path(__file__).parent)
+        # print(f"{self.dataset_root}/{self.kind}/Dataset.csv")
+
+        self.cwd = Path(__file__).parent
+        self.datapath = os.path.join(self.cwd, self.dataset_root, self.kind)
+
+        path = os.path.join(self.cwd, self.dataset_root, f'{self.kind}.csv')
+
+        if not os.path.exists(path):
+            print("Creating CSV")
+            self._create_csv(os.path.join(self.cwd, self.dataset_root, self.kind))
+
+        # print(path)
+
+        self.dataset = pd.read_csv(path)
+        self.len = self.dataset.shape[0]
         # Produce a csv with n values
 
 
     def __len__(self):
-        pass
+        return self.len
 
     def __getitem__(self, idx):
-        pass
+        path, classification = self.dataset.iloc[idx]
+        img = Image.open(os.path.join(self.datapath,path))
+
+        if self.transform:
+            return self.transform(img), classification
+        return img, classification
+        
+
+    def _create_csv(self, path):
+        real = pd.DataFrame(os.listdir(os.path.join(path,"Real")))
+        fake = pd.DataFrame(os.listdir(os.path.join(path,"Fake")))
+
+        real = "Real/" + real
+        fake = "Fake/" + fake
+
+        real["Class"] = 0
+        fake["Class"] = 1
+
+        dataset = pd.concat([real, fake])
+
+        dataset = dataset.rename(columns={0:"Path"})
+        # dataset = dataset.reset_index(drop=True)
+
+        dataset.to_csv(f"{self.cwd}/{self.dataset_root}/{self.kind}.csv", index=False, header=True, encoding="utf8")
+
+
+def main():
+    test = ImageDataset()
+    print(len(test))
+    plt.imshow(test[5][0])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
